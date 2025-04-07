ba300c9bf6c1116ce4f0bc6788810b5092e6d046
Ryan Ruas Pires <ryanruaspires@gmail.com>
2025-04-07 12:29:54 -0300

    New class (Cientific Calculator)

---
+++ WindowsFormsApp1/Minhas classes/clsCalcCientifica.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1.Minhas_classes
+{
+    internal class clsCalcCientifica:clsCalculadoraOO
+    {
+        public string Comparar()
+        {
+            return "";
+        }
+        public string ParImpar()
+        {
+            return "";
+        }
+    }
+}
